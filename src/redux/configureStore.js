0a86e838d069208fbe835df0a818924e8753090f
gwumex <42099116+gwumex@users.noreply.github.com>
2022-12-08 14:07:46 +0100

    react redux form revisited

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,13 @@
 import { applyMiddleware, createStore } from "redux";
+import { combineReducers } from "redux";
 import {Dishes} from './dishes';
 import {Comments} from './comments';
 import {Leaders} from './leaders';
 import {Promotions} from './promotions';
-import { combineReducers } from "redux";
+import {createForms} from 'react-redux-form'
 import thunk from 'redux-logger';
 import logger from 'redux-thunk';
-
+import { InitialFeedback } from "./forms";
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -14,9 +15,13 @@ export const ConfigureStore = () => {
             dishes: Dishes,
             comments: Comments,
             promotions: Promotions,
-            leaders: Leaders
+            leaders: Leaders,
+            ...createForms({
+                feedback: InitialFeedback
+            })
        }),
        applyMiddleware(thunk, logger)
     );
     return store;
 }
+
